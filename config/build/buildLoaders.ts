9521ddf91b47eda81272e96617d459081cf06390
Kostya Lapatin <klrotex11@gmail.com>
2023-09-27 00:12:45 +0300

    config: module.scss support

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,22 +2,32 @@ import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
-export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
-      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      "css-loader",
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (path: string) => Boolean(path.includes('.module.')),
+            localIdentName: isDev ? '[path][name]__[local]': '[hash:base64:8]'
+          }
+        }
+      },
+      // "css-loader",
       "sass-loader",
     ],
   }
+
   // Без ts нужен babel для react
   const tsLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   }
-
 
   return [
     tsLoader,
