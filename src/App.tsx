c48ea9afe1888c580d2da20bc7ea902233afa328
Kostya Lapatin <klrotex11@gmail.com>
2023-09-27 19:54:19 +0300

    config: add react-router-dom, base routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import {Counter} from "./components/Counter/Counter";
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPage, MainPage} from "./pages";
+import {Suspense} from "react";
+
 
 const App = () => {
   return (
     <div className={'App'}>
-      <Counter/>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={'...loading'}>
+        <Routes>
+          <Route path={'/'} element={<MainPage/>}/>
+          <Route path={'/about'} element={<AboutPage/>}/>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,7 @@
+import {lazy} from "react";
+
+export const AboutPageAsync = lazy(() => new Promise(resolve => {
+  // Искусственная задержка для тестов
+  // @ts-ignore
+  setTimeout(() => resolve(import('./AboutPage')), 1500)
+}));
