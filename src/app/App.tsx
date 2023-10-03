8d2f139315c8f1a336af21f4988204533cf7984d
Kostya Lapatin <klrotex11@gmail.com>
2023-10-03 20:10:56 +0300

    feature: Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,18 @@
 import React from "react";
-import {Link} from "react-router-dom";
 
 import {classNames} from "shared/lib/classNames/classNames";
 import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 import {useTheme} from "app/providers/ThemeProvider";
-
-import './styles/index.scss'
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import './styles/index.scss'
 
 const App = () => {
   const {theme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
+      <Navbar/>
       <AppRouter/>
       <ThemeSwitcher/>
     </div>
