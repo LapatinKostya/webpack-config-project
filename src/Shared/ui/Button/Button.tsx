d2cb871968f9d15286b4289424991013ddf7e293
Kostya Lapatin <klrotex11@gmail.com>
2023-10-28 14:35:33 +0300

    feature: Button ui component

---
+++ src/Shared/ui/Button/Button.tsx
@@ -0,0 +1,56 @@
+import { classNames, Mods } from 'shared/lib/classNames/classNames';
+import { ButtonHTMLAttributes, FC } from 'react';
+import s from './Button.module.scss';
+
+export enum ButtonTheme {
+    CLEAR = 'clear',
+    KRINKY = 'krinky',
+    BACKGROUND = 'background',
+    BACKGROUND_INVERTED = 'backgroundInverted',
+}
+
+export enum ButtonSize {
+    M = 'size_m',
+    L = 'size_l',
+    XL = 'size_xl',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    className?: string;
+    theme?: ButtonTheme;
+    square?: boolean;
+    size?: ButtonSize;
+    disabled?: boolean;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+    const {
+        className,
+        children,
+        theme = ButtonTheme.CLEAR,
+        square,
+        disabled,
+        size = ButtonSize.M,
+        ...otherProps
+    } = props;
+
+    const mods: Mods = {
+        [s.square]: square,
+        [s.disabled]: disabled,
+    };
+
+    return (
+        <button
+            type="button"
+            disabled={disabled}
+            className={classNames(
+                s.Button,
+                mods,
+                [className, s[theme], s[size]],
+            )}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};
