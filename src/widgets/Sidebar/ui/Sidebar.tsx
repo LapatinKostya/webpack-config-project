1daf52b470a11c571484923f47ec4eb9d57da51e
Kostya Lapatin <klrotex11@gmail.com>
2023-11-24 18:37:38 +0300

    test: Button, Sidebar

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -3,12 +3,15 @@ import s from './Sidebar.module.scss'
 import { useState } from 'react'
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
 import { LangSwitcher } from 'widgets/LangSwitcher'
+import { useTranslation } from 'react-i18next'
+import { Button } from 'shared/ui/Button/Button'
 
 interface SidebarProps {
   className?: string
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
+  const [t] = useTranslation()
   const [collapsed, setCollapsed] = useState(false)
 
   const mods: Mods = {
@@ -19,8 +22,13 @@ export const Sidebar = ({ className }: SidebarProps) => {
   }
 
   return (
-    <div className={classNames(s.Sidebar, mods, [className])}>
-      <button onClick={onToggle}>toggle</button>
+    <div
+      data-testid={'sidebar'}
+      className={classNames(s.Sidebar, mods, [className])}
+    >
+      <Button data-testid='sidebar-toggle' onClick={onToggle}>
+        {t('toggle')}
+      </Button>
       <div className={s.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={s.lang} />
