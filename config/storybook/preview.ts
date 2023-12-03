42378b38e1768d3d9341253a80922f606ac2bcf7
Kostya Lapatin <klrotex11@gmail.com>
2023-12-04 00:50:46 +0300

    test: themeDecorator sb

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react'
 import { StyleDecorator } from 'shared/config/decorator/StyleDecorator/StyleDecorator'
+import { ThemeDecorator } from 'shared/config/decorator/ThemeDecorator/ThemeDecorator'
+import { Theme } from 'app/providers/ThemeProvider'
 
 const preview: Preview = {
   parameters: {
@@ -11,7 +13,7 @@ const preview: Preview = {
       },
     },
   },
-  decorators: [StyleDecorator],
+  decorators: [StyleDecorator, ThemeDecorator(Theme.LIGHT)],
 }
 
 export default preview
