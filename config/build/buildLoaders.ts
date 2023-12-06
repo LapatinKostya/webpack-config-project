3c0a52c84317973bc05ec9a8175dd3d60a14dba1
Kostya Lapatin <klrotex11@gmail.com>
2023-12-06 14:36:03 +0300

    config: svg loader for storybook

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,12 +1,11 @@
 import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import { buildCssLoaders } from './loaders/buildCssLoaders'
+import { buildSvgLoaders } from './loaders/buildSvgLoaders'
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-  const svgLoader = {
-    test: /\.svg$/,
-    use: ['@svgr/webpack'],
-  }
+  const svgLoader = buildSvgLoaders()
+
   const fileLoader = {
     test: /\.(png|jpe?g|gif)$/i,
     use: [
--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,7 +1,8 @@
-import webpack from 'webpack'
+import webpack, { RuleSetRule } from 'webpack'
 import { BuildPath } from '../build/types/config'
 import path from 'path'
 import { buildCssLoaders } from '../build/loaders/buildCssLoaders'
+import { buildSvgLoaders } from '../build/loaders/buildSvgLoaders'
 
 export default ({ config }: { config: webpack.Configuration }) => {
   const paths: BuildPath = {
@@ -12,6 +13,17 @@ export default ({ config }: { config: webpack.Configuration }) => {
   }
   config.resolve.modules.push(paths.src)
   config.resolve.extensions.push('.ts', '.tsx')
+
+  // Убираем из fileLoader обработку svg
+  config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+    if (/svg/.test(rule.test as string)) {
+      return { ...rule, exclude: /\.svg$/i }
+    }
+
+    return rule
+  })
+
+  config.module.rules.push(buildSvgLoaders())
 
   config.module.rules.push(buildCssLoaders(true))
 
