08845ea458bd1f6de95f377945f7b51e04159d48
Kostya Lapatin <klrotex11@gmail.com>
2023-12-06 12:57:23 +0300

    test: Button.stories.tsx

--- src/Shared/ui/Button/Button.stories.tsx
+++ src/Shared/ui/Button/Button.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { Button, ButtonSize, ButtonTheme } from './Button'
+import { ThemeDecorator } from 'shared/config/decorator/ThemeDecorator/ThemeDecorator'
+import { Theme } from 'app/providers/ThemeProvider'
 
 const meta = {
   title: 'shared/Button',
@@ -25,6 +27,8 @@ export const Krinky: Story = {
     theme: ButtonTheme.KRINKY,
   },
 }
+Krinky.decorators = [ThemeDecorator(Theme.DARK)]
+
 export const Outline: Story = {
   args: {
     children: 'Outline',
