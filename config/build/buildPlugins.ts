579bc191ffd375243dcd5fcbdca8792807d95814
Kostya Lapatin <klrotex11@gmail.com>
2023-12-06 21:05:18 +0300

    fix: ci

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,7 +8,7 @@ export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new webpack.ProgressPlugin(),
 
     new HTMLWebpackPlugin({
@@ -21,6 +21,9 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new BundleAnalyzerPlugin({ openAnalyzer: false }),
   ]
+  if (isDev) {
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
+  }
+  return plugins
 }
