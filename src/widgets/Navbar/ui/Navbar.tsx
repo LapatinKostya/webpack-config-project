17e2a4413fc51b34401952a1ed6390f9f26c4781
Kostya Lapatin <klrotex11@gmail.com>
2023-12-07 18:36:32 +0300

    feature: Sidebar modded

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import s from './Navbar.module.scss'
-import { AppLink } from 'shared/ui/AppLink/AppLink'
-import { useTranslation } from 'react-i18next'
 import { BugButton } from 'app/providers/ErrorBoundary'
 
 interface NavbarProps {
@@ -9,15 +7,9 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-  const { t } = useTranslation()
-
   return (
     <div className={classNames(s.Navbar, {}, [className])}>
-      <div className={s.navigate}>
-        <AppLink to={'/'}>{t('Main page')}</AppLink>
-        <AppLink to={'/about'}>{t('About page')}</AppLink>
-        <BugButton />
-      </div>
+      <BugButton />
     </div>
   )
 }
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -6,12 +6,19 @@ import { LangSwitcher } from 'widgets/LangSwitcher'
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button'
 import ArrowRight from 'shared/assets/icons/arrow-right.svg'
 import ArrowLeft from 'shared/assets/icons/arrow-left.svg'
+import { AppLink } from 'shared/ui/AppLink/AppLink'
+import { useTranslation } from 'react-i18next'
+import MainPageIcon from 'shared/assets/icons/main-page-icon.svg'
+import AboutPageIcon from 'shared/assets/icons/about-page-icon.svg'
+import { IconWrap } from 'shared/ui/IconWrap/IconWrap'
+import { RouterPath } from 'shared/config/routeConfig/routeConfig'
 
 interface SidebarProps {
   className?: string
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
+  const { t } = useTranslation()
   const [collapsed, setCollapsed] = useState(false)
 
   const mods: Mods = {
@@ -26,6 +33,21 @@ export const Sidebar = ({ className }: SidebarProps) => {
       data-testid={'sidebar'}
       className={classNames(s.Sidebar, mods, [className])}
     >
+      <div className={s.navigateItems}>
+        <AppLink to={RouterPath.main} className={s.navigateItem}>
+          <IconWrap className={s.icon}>
+            <MainPageIcon />
+          </IconWrap>
+          {!collapsed && 'Main page'}
+        </AppLink>
+        <AppLink to={RouterPath.about} className={s.navigateItem}>
+          <IconWrap className={s.icon}>
+            <AboutPageIcon />
+          </IconWrap>
+          {!collapsed && 'About page'}
+        </AppLink>
+      </div>
+
       <Button
         data-testid='sidebar-toggle'
         onClick={onToggle}
